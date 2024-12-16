2ee6bca7db59a0b0a15a86d22040335191e359e3
jgarvel076 <jgarvel076@g.educaand.es>
2024-12-16 13:18:09 +0100

    tarea1

--- Tema 9 (ajax)/tarea/bd.php
+++ Tema 9 (ajax)/tarea/bd.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Archivo usado para realizar conexiones (tomado de la asignatura DWES)
- */
-
- // Creamos una clase llamada conexión, en la que generamos una conexión a una base de datos empleando PHP, usando para ello un objeto de la clase PDO,
- // que cuenta con métodos específicos para realizar dichas conexiones.
+ //clase conexión, generamos una conexión a la base de datos
  class Conexion {
         public $pdo;
         public function __construct()
         {
             try {
-                // Indicamos el host y el nombre de la base de datos
+                // indicamos el host y el nombre de la base de datos
                 $dsn = "mysql:host=localhost;dbname=tema10";
                 $options = [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -18,11 +13,9 @@
                     PDO::ATTR_EMULATE_PREPARES => false,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
                 ];
-                // Añadimos a la propiedad de la clase un objeto de la clase PDO, pasandole como parametros $dsn (string con datos de host y BBDD), usuario, contraseña y el array
-                // $options con los múltiples atributos
                 $this->pdo = new PDO($dsn, "root", "", $options);
             } catch (PDOException $e) {
-                // Definimos la información del error y cerramos el script en caso de saltar excepción
+                // definimos la información del error
                 echo "ERROR BASE DE DATOS: ";
                 echo "<HR>";
                 echo "Mensaje de Error:      ". $e->getMessage(). "<BR>";
@@ -35,45 +28,44 @@
         }
     }
 
-    // Creamos una estructura en la que, según lo que se solicite a través del método GET, realiza una u otra consulta
     if (isset($_GET['nombresId'])) {
-        // Vamos a crear la consulta correspondiente
+        // creamos la consulta 
         $sql = "SELECT id, nombre FROM tema10.datos";
 
-        // Realizamos la conexión y preparamos la consulta
+        // realizamos la conexión
         $conexion = new Conexion();
         $pdostmt = $conexion->pdo->prepare($sql);
 
-        // Ejecutamos la consulta
+        // ejecutamos la consulta
         $pdostmt->execute();
 
-        // Indicamos el tipo de fetch para el resultado
+        // indicamos el tipo de fetch para el resultado
         $result = $pdostmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // Devolvemos el resultado, convertido a JSON
+        // devolvemos el resultado como JSON
         echo json_encode($result);
 
         
     } else if (isset($_GET['id'])){
-        // Capturamos el valor enviado mediante el método GET
+        // capturamos el valor del método GET
         $id = $_GET['id'];
-        // Vamos a crear la consulta correspondiente
+        // creamos la consulta 
         $sql = "SELECT id, nombre, apellidos, ciudad FROM tema10.datos WHERE id = :id";
 
-        // Realizamos la conexión y preparamos la consulta
+        // realizamos la conexión
         $conexion = new Conexion();
         $pdostmt = $conexion->pdo->prepare($sql);
 
-        //Vinculamos la variable
+        // vinculamos la variable
         $pdostmt->bindParam(':id', $id, PDO::PARAM_INT);
 
-        // Ejecutamos la consulta
+        // ejecutamos la consulta
         $pdostmt->execute();
 
-        // Indicamos el tipo de fetch para el resultado
+        // indicamos el tipo de fetch para el resultado
         $result = $pdostmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // Devolvemos el resultado, convertido a JSON
+        // devolvemos el resultado, convertido a JSON
         echo json_encode($result);
     }
 ?>
